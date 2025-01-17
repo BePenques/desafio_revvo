aec54b93e3a22d4412b38f9b2fac5f025c200ac6
Betiza Barreira <be.penques@hotmail.com>
2025-01-17 10:55:57 -0300

    add font roboto to project

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ $pages = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <title>LEO Cursos</title>
    
 </head>

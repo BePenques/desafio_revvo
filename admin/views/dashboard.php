9ab33c3e2bf6941f04dee9ff3038ce080433dff5
Betiza Barreira <be.penques@hotmail.com>
2025-01-18 19:24:57 -0300

    alter dashboard title

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard - Cursos</title>
+    <title>Admin Dashboard</title>
 </head>
 <body>
     <div class="dashboard-container">
-        <h1>Admin Dashboard - Cursos</h1>
+        <h1>Admin Dashboard</h1>
         <div class="button-container">
-            <a href="index.php?page=course_create" class="button">Cadastrar Curso</a>
+            <a href="index.php?page=create" class="button">Cadastrar Curso</a>
             <a href="index.php?page=slideshow_create" class="button">Cadastrar Slideshow</a>
             <a href="../index.php?page=home" class="button">Retornar à Home</a>
         </div>

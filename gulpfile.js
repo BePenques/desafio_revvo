cda848885f52e395b5ac4ebcbc109eb1af55afc8
Betiza Barreira <be.penques@hotmail.com>
2025-01-18 15:24:28 -0300

    change of gulpfile directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function() {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('./assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('./assets/sass/**/*.scss', gulp.series('sass'));
 });
 
 gulp.task('default', gulp.series('sass', 'watch'));

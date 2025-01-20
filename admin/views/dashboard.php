82ccb35458930af8e4351b39ff5d9602254e1513
Betiza Barreira <be.penques@hotmail.com>
2025-01-20 16:48:21 -0300

    dropdown menu

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -9,7 +9,7 @@
     <div class="dashboard-container">
         <h1>Admin Dashboard</h1>
         <div class="button-container">
-            <a href="index.php?page=create" class="button">Cadastrar Curso</a>
+            <a href="/admin/index.php?page=create" class="button">Cadastrar Curso</a>
             <a href="index.php?page=slideshow_create" class="button">Cadastrar Slideshow</a>
             <a href="../index.php?page=home" class="button">Retornar à Home</a>
         </div>
--- php/partials/header.php
+++ php/partials/header.php
@@ -1,5 +1,5 @@
 <header class="site-header">
-    <div class="container">
+    <div class="container-header">
         <div class="logo">           
             <img src="/assets/images/logo.png" alt="revvo Logo">
         </div>
@@ -13,12 +13,35 @@
             </form>
         </div>
 
-        <div class="user-info">
-            <img class="user-profile" src="https://github.com/BePenques.png" alt="Avatar do usuário">
-            <div>
-                <span>Seja bem-vindo</span>
-                <span>Betiza Penques</span>
-            </div>
-        </div>
+        <nav>
+            <ul class="menu">
+                <li class="dropdown">
+                    <a href="#" class="dropbtn" onclick="toggleDropdown(event)">
+                        <div class="user-info">
+                            <img class="user-profile" src="https://github.com/BePenques.png" alt="Avatar do usuário">
+                            <div>
+                                <div class="dd-infos">
+                                    <span>Seja bem-vindo,</span>
+                                    <span>Betiza Penques</span>
+                                </div>
+                                <i class="fas fa-chevron-down"></i>
+                            </div>
+                            
+                        </div>
+                    </a>
+                    <div class="dropdown-content">
+                        <a href="/">Home</a>
+                        <a href="/admin">Admin</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
     </div>
+    <script>
+        function toggleDropdown(event) {
+            event.preventDefault();
+            var dropdownContent = event.currentTarget.nextElementSibling;
+            dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+        }
+    </script>
 </header>
